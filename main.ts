c55f600b94311a94004a6f6a30f10e1ae1d93ca8
Nathan George <kosmosisdire@gmail.com>
2022-11-15 00:46:20 -0500

    1.1.0 commit

--- main.ts
+++ main.ts
@@ -24,7 +24,7 @@ export default class HTMLExportPlugin extends Plugin {
 	mathStylesLoaded : boolean = false;
 	
 
-	autoDownloadExtras = false;
+	autoDownloadExtras = true;
 
 	webpagejsURL : string = "https://raw.githubusercontent.com/KosmosisDire/obsidian-webpage-export/master/webpage.js";
 	pluginStylesURL : string = "https://raw.githubusercontent.com/KosmosisDire/obsidian-webpage-export/master/plugin-styles.css";
